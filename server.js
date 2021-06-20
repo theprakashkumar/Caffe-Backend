c796948d9ba0609ec9b6a2e8fe8f8ead3bec79a9
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-06-20 21:33:07 +0530

    fix typo

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.use("/cart", cartRouter);
 app.use("/wishlist", wishlistRouter);
 
 // listen on port 5000
-app.listen(process.env.port || 5000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log("Server Started 🙌");
 });
