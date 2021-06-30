04cad438690c1c33ce7430cd1a920bafd2b78ab8
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-06-30 11:32:58 +0530

    refactor: added auth middleware to get user details

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const authenticate = require("../middleware/authenticate");
 
 const {
     getUserLogin,
@@ -10,6 +11,9 @@ const { getUserById } = require("../controllers/params.controllers");
 
 router.post("/login", getUserLogin);
 router.post("/signup", createNewUser);
+
+// middleware for authentication
+router.use(authenticate);
 
 router.param("id", getUserById);
 router.get("/:id", getUserDetails);
